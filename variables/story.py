23247b2d2cbffae69039e67fc1b0b0c9dc93b47e
Israel <israelmilla96@gmail.com>
2022-09-18 17:58:21 -0600

    Practicing variables

---
+++ variables/story.py
@@ -0,0 +1,14 @@
+character_name = "John"
+Character_age = "35"
+is_male = True
+print("There was once was a man named " + character_name)
+print("He was " + Character_age + " years old")
+
+character_name = "Mike"
+Character_age = "50"
+
+print("There was once was a man named " + character_name)
+print("He was " + Character_age + " years old")
+
+
+
