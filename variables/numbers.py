a8611d7411c0b6eb82a75947562a091db0d679da
Israel <israelmilla96@gmail.com>
2022-09-18 21:10:01 -0600

    More Learning:

---
+++ variables/numbers.py
@@ -0,0 +1,15 @@
+from math import *
+
+print(3 + 5) #Prints the value of the addition
+print(10 % 3) #10 mod 3 spits out the remainder from 10/3
+
+my_num = 5
+print(my_num)
+print(str(my_num) + " seconds") #Prints number as string. Needed to add text to print statement
+
+print(pow(3, 2)) # prints 3 to 2nd power
+print(max(4,6)) #prints the max of the values input. min is opposite
+
+print(floor(3.7)) #round number down no matter what
+print(sqrt(36)) #print square root of value
+
---
+++ variables/strings.py
@@ -0,0 +1,12 @@
+phrase = "Vim Text Editor"
+print(phrase + " is amazing") #Concadination
+print(phrase.lower())
+print(phrase.upper())
+print(phrase.isupper()) #Checks if upper, return boolean statement
+print(phrase.upper().isupper()) #Can append checks
+print(len(phrase)) #print the length of phrase
+print(phrase[10]) #print the index of the number in the word. Starts with 0
+print(phrase.index("dit")) #Finds location of where things are
+
+
+
